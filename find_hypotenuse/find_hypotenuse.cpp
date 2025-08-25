cc5d998472eabe07010c91818b33110e7ece2f80
yoojanghun <yoojanghun@naver.com>
2025-08-25 16:45:33 +0900

    find hypotenuse

---
+++ find_hypotenuse/find_hypotenuse.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <cmath>
+
+int main() {
+
+  double a;
+  double b;
+  double c;
+
+  std::cout << "Enter side A: ";
+  std::cin >> a;
+
+  std::cout << "Enter side B: ";
+  std::cin >> b;
+
+  a = pow(a, 2);
+  b = pow(b, 2);
+  c = sqrt(a + b);
+
+  std::cout << "side C: " << c;
+
+  return 0;
+}
