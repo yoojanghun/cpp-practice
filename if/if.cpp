1a7bd3acca0d8971fbffb7e4d666581ce16e3fbd
yoojanghun <yoojanghun@naver.com>
2025-08-25 16:51:46 +0900

    if

---
+++ if/if.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+int main() {
+  // if statements: do something if a condition is true.
+  //                if not, then don't do it.
+
+  int age;
+
+  std::cout << "Enter your age: ";
+  std::cin >> age;
+
+  if(age >= 100) {
+    std::cout << "You are too old to enter this site";
+  }
+  else if(age >= 18) {
+    std::cout << "Welcome to the site!!";
+  }
+  else if(age < 0) {
+    std::cout << "You haven't been born yet";
+  }
+  else {
+    std::cout << "You are not old enough";
+  }
+  
+  return 0;
+}
