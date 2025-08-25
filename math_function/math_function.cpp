c9d7a01d06e14ec797595e3eb0da03edd0e5a0c5
yoojanghun <yoojanghun@naver.com>
2025-08-25 16:37:42 +0900

    math function

---
+++ math_function/math_function.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cmath>
+
+int main() {
+
+  double x = 3.14;
+  double y  = 4;
+  double z;
+
+  // z = std::max(x, y);
+  // z = std::min(x, y);
+
+  // 아래는 cmath 라이브러리 추가해야 함
+  // z = pow(2, 3);
+  // z = sqrt(9);
+  // z = abs(-4);
+  // z = round(x);
+  // z = ceil(x);
+  // z = floor(x);
+
+  std::cout << z;
+
+  return 0;
+}
