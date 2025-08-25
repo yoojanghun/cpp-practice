37e31c3d1d5ca0e92f068984f6349b140a4160fb
yoojanghun <yoojanghun@naver.com>
2025-08-25 10:43:55 +0900

    using namespace std

--- namespace/namespace.cpp
+++ namespace/namespace.cpp
@@ -7,19 +7,24 @@ namespace second{
   int x = 2;
 }
 
+
 int main() {
   // Namespace = provides a solution for preventing name conflicts 
   //             in large projects. Each entity needs a unique name.
   //             A namespace allows for identically named entities 
   //             as long as the namespaces are different.
+  
+  // using namespace를 사용하면 특정 namespace안 요소들을 접두사 없이 쓸 수 있다.
+  // 하지만 std안에는 많은 함수, 클래스, 변수 이름이 있기 때문에, 충돌 문제 발생 가능. 
+  // 그래서 필요한 경우에만 아래처럼 쓸 수 있다.
+  // using std::string;
+  // using std::cout;
 
-  // int x = 0;
-  using namespace first;    // first namespace안에 있는 이름들을 꺼내 쓴다.
-  // 단 int x = 0을 하면 지역변수 > 전역변수(namespace 포함) 우선순위가 적용되어, 출력은 0
+  using namespace std;      // std 안 요소들을 접두사 없이 쓸 수 있다.
+  
+  string hello = "hello world";
 
-  std::cout << x << "\n";
-  std::cout << first::x << "\n";
-  std::cout << second::x << "\n";
+  cout << hello;
 
   return 0;
 }
